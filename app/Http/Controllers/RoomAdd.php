6e88a8dff4e3c0ee3305be557d570705ba463cce
ahtesham11 <33203348+ahtesham11@users.noreply.github.com>
2024-08-28 13:19:45 +0500

    mai

--- app/Http/Controllers/RoomAdd.php
+++ app/Http/Controllers/RoomAdd.php
@@ -4,7 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Room;
+
+
 class RoomAdd extends Controller
 {
-    //
+    public function index(){
+        $rooms=Room::all();
+        return view('admin.rooms',compact($rooms));
+    }
 }
